dcfd34fb839bcc341b944431bb08a2d73bed020c
Rubén Vásquez <ruvasqm@gmail.com>
2025-01-30 15:04:55 -0400

    fix pr workflow (#3)

--- .github/workflows/deploy-pr-script.sh
+++ .github/workflows/deploy-pr-script.sh
@@ -3,10 +3,10 @@ SECTION="env.$PR_NUMBER"
 grep -q "^\[$SECTION\]$" wrangler.toml || {
   {
     printf "\n[%s]" "$SECTION"
-    printf "\nworkers_dev = true"
+    printf "\nworkers_dev = true\n"
   } >>wrangler.toml
 }
 bunx wrangler d1 list | grep -q "db-$PR_NUMBER" || {
-  bunx wrangler d1 create "db-$PR_NUMBER" | tail -n 5 | sed "s/^\\[\\[d1_databases\\]\\]/\\[\\[$D1_SECTION\\]\\]/" >>wrangler.toml
+  bunx wrangler d1 create "db-$PR_NUMBER" | tail -n 5 | sed "s/^\\[\\[d1_databases\\]\\]/\\[\\[$SECTION.d1_databases\\]\\]/" >>wrangler.toml
 }
 cat wrangler.toml
