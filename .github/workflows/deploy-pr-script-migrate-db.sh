0dbbac3be215b1789de2e30145ec7d98c3d68e53
Ruben Vasquez <ruvasqm@gmail.com>
2025-01-30 15:03:13 -0400

    fix hardcoded pr number

--- .github/workflows/deploy-pr-script-migrate-db.sh
+++ .github/workflows/deploy-pr-script-migrate-db.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
-DB_ID=$(bunx wrangler d1 list | sed -nE 's/^.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*db-pr-1.*/\1/p')
+DB_ID=$(bunx wrangler d1 list | sed -nE "s/^.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*db-$PR_NUMBER.*/\1/p")
 grep -q "^\$DB_ID$" wrangler.toml || {
   {
     printf "\n[[env.%s.d1_databases]]" "$PR_NUMBER"
